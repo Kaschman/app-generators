1a1ce825ab6c42937b3ecbda9fc9da8dd536bb8b
Steve Kaschinske <steve.kaschinske@gmail.com>
2019-07-28 10:26:39 -0600

    Add ability to develop against a Create React App cache

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # generator-react-app
 Yeoman generator for creating React applications
 
-Getting started:
+## Getting started:
+### Running the Generator
 1. `$ yarn global add yo` if you haven't installed Yeoman yet.
 2. `$ yarn install`
 3. `$ yarn build`
 4. `$ yarn link` from this project's root directory to make the generator discoverable by Yeoman
-5. `$ yo react-app` from a different directory - the generator does not work if you have uncommitted git files.
+5. `$ yo react-app` to run the generator - note: the generator does not work if you have uncommitted git files in the directory where the command is run.
+
+### Developing the Generator
+Create React App takes awhile to run. When developing the generator, you can use a cached version of the Create React App boilerplate to speed up generation time.
+1. `$ yarn global add yo` if you haven't installed Yeoman yet.
+2. `$ yarn install`
+3. `$ yarn build`
+4. `$ yarn link` from this project's root directory to make the generator discoverable by Yeoman
+5. `$ yarn update:cra-cache` to load the CRA cache. Run again to update it if needed. Note: you cannot have uncommitted git files to run this script.
+6. `$ yarn generate` to run the generator with the CRA cache targeting the `/sample-react-app` directory
 
 Some notes on [working with Yeoman in ES6](http://mammal.io/articles/yeoman-generators-es6/).
 
@@ -26,10 +36,13 @@ Some notes on [working with Yeoman in ES6](http://mammal.io/articles/yeoman-gene
 - Template README
 - cache or CRA
 
+## Performance
+Even using the `--cra-cache` flag can be a bit slow. The project using the [cpy](https://github.com/sindresorhus/cpy) library at the moment to copy the cache to the destination directory. [recursive-copy](https://github.com/timkendrick/recursive-copy) was tried as well but it was about 75% slower.
+
 ## Linting
 - https://prettier.io/docs/en/integrating-with-linters.html
 - https://github.com/prettier/prettier-eslint/issues/101
 - https://stackoverflow.com/questions/46201647/prettier-airbnbs-eslint-config
 
-# Babel Config
+## Babel Config
 `babel-plugin-add-module-exports` is needed for the old `module.exports` functionality (see this [issue](https://github.com/yeoman/yo/issues/391)).
--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 import Generator from 'yeoman-generator'
 import chalk from 'chalk'
+import cliProgress from 'cli-progress'
+import cpy from 'cpy'
 import fs from 'fs-extra'
 import _ from 'lodash'
 import { execSync } from 'child_process'
@@ -20,6 +22,14 @@ const getGitStatus = () => {
 }
 
 class ReactAppGenerator extends Generator {
+  // arguments and options are defined in the constructor
+  constructor(args, opts) {
+    super(args, opts)
+
+    // use Create React App cache, set by `--cra-cache`
+    this.option('cra-cache')
+  }
+
   // Prompting Priority
   get prompting() {
     return {
@@ -64,6 +74,9 @@ class ReactAppGenerator extends Generator {
       // If there are untracked changes, Create React App eject will fail
       // Catch the error now before running create-react-app
       checkGitStatus() {
+        const { craCache } = this.options
+        if (craCache) return
+
         const gitStatus = getGitStatus()
         if (gitStatus) {
           this.log(
@@ -75,6 +88,16 @@ class ReactAppGenerator extends Generator {
           process.exit(1)
         }
       },
+
+      // If --cra-cache option is used, make sure we have a cra-cache
+      async checkForCraCache() {
+        const { craCache } = this.options
+        if (!craCache) return
+        const exists = await fs.pathExists(this.destinationPath('cra-cache'))
+        if (!exists) {
+          this.env.error("No CRA cache exists. Please run 'yarn update:cra-cache' to generate one.")
+        }
+      },
     }
   }
 
@@ -82,11 +105,17 @@ class ReactAppGenerator extends Generator {
   get default() {
     return {
       initWithCreateReactApp() {
+        const { craCache } = this.options
+        if (craCache) return
+
         this.log(chalk.cyanBright('\nGenerating intial application using Create React App...\n'))
         this.spawnCommandSync('npx', ['create-react-app', this.name])
       },
 
       ejectCreateReactApp() {
+        const { craCache } = this.options
+        if (craCache) return
+
         this.destinationRoot(this.destinationPath(this.name))
         this.log(chalk.cyanBright('\nEjecting Create React App\n'))
         const done = this.async() // spawnCommandSync doesn't like to pipe so use this strategy to handle async
@@ -102,6 +131,26 @@ class ReactAppGenerator extends Generator {
           }
           done()
         })
+      },
+
+      async useCreateReactAppCache() {
+        const { craCache } = this.options
+        if (!craCache) return
+
+        this.log(chalk.cyanBright('\nCopying CRA cache'))
+        const bar = new cliProgress.Bar({ etaBuffer: 100 }, cliProgress.Presets.legacy)
+
+        let start = false
+        await cpy(this.destinationPath('cra-cache'), this.destinationPath(this.name)).on('progress', progress => {
+          if (!start) {
+            bar.start(progress.totalFiles, 0)
+            start = true
+          }
+          bar.update(progress.completedFiles)
+        })
+        bar.stop()
+        this.log(chalk.cyanBright('Copying Complete'))
+        this.destinationRoot(this.destinationPath(this.name))
       },
     }
   }
