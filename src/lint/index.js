4a6b53c397f6e7fb24b2abd0772592d19b9e9cce
Steve Kaschinske <steve.kaschinske@gmail.com>
2019-08-12 12:22:19 -0600

    Add prettier to generated react project

--- src/lint/index.js
+++ src/lint/index.js
@@ -13,29 +13,37 @@ class LintGenerator extends Generator {
         this.log(chalk.cyanBright('\nAdding ESLint and Prettier'))
         const pkg = await fs.readJson(this.destinationPath('package.json'))
         extend(pkg, {
-          dependencies: {
+          devDependencies: {
             'eslint-config-airbnb': '^17.1.1',
+            'eslint-config-prettier': '^6.0.0',
             'eslint-plugin-import': '^2.18.2',
             'eslint-plugin-jsx-a11y': '^6.2.3',
+            'eslint-plugin-prettier': '^3.1.0',
             'eslint-plugin-react': '^7.14.3',
+            prettier: '^1.18.2',
           },
           scripts: {
             fix: 'yarn lint --fix',
             lint: 'npx eslint . --ext .js,.jsx',
           },
         })
+        delete pkg.dependencies['eslint-plugin-import']
+        delete pkg.dependencies['eslint-plugin-jsx-a11y']
+        delete pkg.dependencies['eslint-plugin-react']
         await fs.writeJSON(this.destinationPath('package.json'), pkg, { spaces: 2 })
       },
 
       async writeConfig() {
         await fs.copy(this.templatePath('template.eslintignore'), this.destinationPath('.eslintignore'))
         await fs.copy(this.templatePath('template.eslintrc.json'), this.destinationPath('.eslintrc.json'))
+        await fs.copy(this.templatePath('template.prettierrc.json'), this.destinationPath('.prettierrc.json'))
         this.log('Copied ESLint template files\n')
       },
 
-      updateJSXFiles() {
-        fs.renameSync(this.destinationPath('src/App.js'), this.destinationPath('src/App.jsx'))
-        this.log('Renamed App.js -> App.jsx')
+      async updateJSXFiles() {
+        await fs.remove(this.destinationPath('src/App.js'))
+        await fs.copy(this.templatePath('template.App'), this.destinationPath('src/App.jsx'))
+        this.log('Copied App.jsx template')
         fs.renameSync(this.destinationPath('src/App.test.js'), this.destinationPath('src/App.test.jsx'))
         this.log('Renamed App.test.js -> App.test.jsx')
       },
